03bbe20a9937a26a310914d9937ffc6508f537ec
Soomlin <112004175+Soomlin@users.noreply.github.com>
2022-08-24 12:19:17 -0400

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Aiden Sumlin");
+		System.out.println("Hello");
+		System.out.println("Saying Hello");
+		System.out.println("08/24/22");
+		System.out.println("CMSC 255 004");
+		System.out.println();
+	}
+}
